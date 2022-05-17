d01278bf26269d6248cf4d85e9e863609b52d55b
Denny Ribeiro <denny.ribeiro@outlook.com>
2022-05-17 19:31:33 -0300

    initial config

---
+++ projeto-integrador-front/src/index.js
@@ -0,0 +1,18 @@
+import { ColorModeScript } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+import {ChakraProvider} from '@chakra-ui/react';
+import { customTheme } from './styles/GlobalStyles';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <StrictMode>
+    <ChakraProvider theme={customTheme}>
+      <ColorModeScript />
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
