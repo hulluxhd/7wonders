3263911e3b99e4b53e745714c7d34742d3c7b936
lucassneiva <lucassneiva.code@gmail.com>
2022-05-19 15:20:22 -0300

    adding location and category to items

--- projeto-integrador-front/src/components/PlaceCard.jsx
+++ projeto-integrador-front/src/components/PlaceCard.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, {
+//  useEffect,
+// useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 function PlaceCard({ place }) {
-  function seeMore(target) {
-    const increaseView = target.parentNode.parentNode;
-    increaseView.style.width = '500px';
-    increaseView.style.position = 'absolute';
-    increaseView.style.margin = '0 auto';
-    increaseView.style.zIndex = '999999';
-  }
+  // const [seeMore, setSeeMore] = useState(false);
+
+  // useEffect(() => {}, [seeMore]);
+
+  // function increaseDiv(target) {
+  //   const increase = document.getElementById('increase');
+  //   const increaseView = target.parentNode.parentNode;
+  //   if (seeMore === false) {
+  //     setSeeMore(true);
+  //     increaseView.id = 'increase';
+  //     increase.style.width = '500px';
+  //     // increase.style.top = '30%';
+  //     increase.style.position = 'absolute';
+  //     // increase.style.margin = '0 auto';
+  //     increase.style.zIndex = '999999';
+  //   } else {
+  //     setSeeMore(false);
+  //     increaseView.removeAttribute('id');
+  //   }
+  // }
   return (
     <section className="place-card">
-      <img className="place-card-image" src={place.imagePath} alt={place.name} />
+      <section style={{ position: 'relative' }}>
+        <img className="place-card-image" src={place.imagePath} alt={place.name} />
+        {/* <p className="place-card-category">{place.category}</p> */}
+        <p className="place-card-location">
+          {place.city}
+          ,
+          {' '}
+          {place.country}
+        </p>
+      </section>
       <section className="place-card-body">
         <header style={{ display: 'flex', justifyContent: 'space-between' }}>
           <h4 className="place-card-name">{place.name}</h4>
           <Rating rating={place.rating} />
         </header>
-        <h5 className="place-card-types">{place.types[0]}</h5>
+        <section className="place-card-types-section">
+          <h5 className="place-card-types">{place.category}</h5>
+          <h5 className="place-card-types">{place.types[0]}</h5>
+        </section>
         <p className="place-card-about">{place.about}</p>
-        <button type="button" className="see-more" onClick={(ev) => seeMore(ev.target)}>See more</button>
+        <button
+          type="button"
+          className="see-more"
+        // onClick={(ev) => increaseDiv(ev.target)}
+        >
+          See more
+
+        </button>
       </section>
     </section>
   );
