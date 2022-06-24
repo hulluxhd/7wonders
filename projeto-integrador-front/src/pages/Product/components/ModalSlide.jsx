f04b57dd4eab625f3c1f332378fbbad306a3a1ff
Peter <peterleite.dev@gmail.com>
2022-06-24 13:25:52 -0300

    delete css file

--- projeto-integrador-front/src/pages/Product/components/ModalSlide.jsx
+++ projeto-integrador-front/src/pages/Product/components/ModalSlide.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './ModalSlide.css';
 
 import {
   Box,
@@ -45,7 +44,34 @@ export default function ModalSlide() {
       navigation
       modules={[FreeMode, Navigation, Thumbs, Pagination, A11y]}
       className="mySwiper2"
+      pagination={{ type: 'fraction', clickable: true }}
     >
+      <SwiperSlide>
+        <Box
+          bgImage="url('https://images.trvl-media.com/hotels/13000000/12080000/12079000/12078999/a4a65579.jpg?impolicy=resizecrop&rw=1200&ra=fit')"
+          bgPosition="center"
+          bgSize="cover"
+          bgRepeat="no-repeat"
+          margin="0 auto"
+          height="70vh"
+          width="80vw"
+        >
+          Slide 1
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box
+          bgImage="url('https://images.trvl-media.com/hotels/13000000/12080000/12079000/12078999/a4a65579.jpg?impolicy=resizecrop&rw=1200&ra=fit')"
+          bgPosition="center"
+          bgSize="cover"
+          bgRepeat="no-repeat"
+          margin="0 auto"
+          height="70vh"
+          width="80vw"
+        >
+          Slide 1
+        </Box>
+      </SwiperSlide>
       <SwiperSlide>
         <Box
           bgImage="url('https://images.trvl-media.com/hotels/13000000/12080000/12079000/12078999/a4a65579.jpg?impolicy=resizecrop&rw=1200&ra=fit')"
